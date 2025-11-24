049d41edb58a5ce2cffbaeb2287df77955535c28
Hajar Moukit <hmoukit@e1r4p15.1337.ma>
2025-11-24 18:00:59 +0100

    adding 07 files

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmoukit <hmoukit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 17:19:28 by hmoukit           #+#    #+#             */
+/*   Updated: 2025/11/24 17:57:07 by hmoukit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+    return ((a < b) ? a : b);
+}
+
+template <typename T>
+T max(T a, T b)
+{
+    return ((a < b) ? b : a);
+}
+
+
+#endif
